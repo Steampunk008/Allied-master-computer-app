a8e154b1a02a2d225a11d7cad9c9b0c0afc2adfa
Steampunk008 <vijayadesha75@gmail.com>
2025-12-10 06:14:32 +0530

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// js/firebase-config.js
+// Export the firebaseConfig so app.js can import it.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPEfNzuHxqLhsiB57FlKRCkoFfShnRMfU",
+  authDomain: "am-chat-1520e.firebaseapp.com",
+  projectId: "am-chat-1520e",
+  storageBucket: "am-chat-1520e.firebasestorage.app",
+  messagingSenderId: "209685016382",
+  appId: "1:209685016382:web:98c4302a7acebf40e9866f",
+  measurementId: "G-KLY2VQH04G"
+};
