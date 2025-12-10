6a66e94c0e29f13658688dd08bc17eb3f49528a2
Steampunk008 <vijayadesha75@gmail.com>
2025-12-10 06:01:11 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPEfNzuHxqLhsiB57FlKRCkoFfShnRMfU",
+  authDomain: "am-chat-1520e.firebaseapp.com",
+  projectId: "am-chat-1520e",
+  storageBucket: "am-chat-1520e.firebasestorage.app",
+  messagingSenderId: "209685016382",
+  appId: "1:209685016382:web:98c4302a7acebf40e9866f",
+  measurementId: "G-KLY2VQH04G"
+};
